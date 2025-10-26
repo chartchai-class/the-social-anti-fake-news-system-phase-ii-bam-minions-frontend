20038545c515575911afb17b34c1dbf7e9a85451
macguide <Saengchai_fung@cmu.ac.th>
2025-10-27 01:13:07 +0700

    Add home page button on detail view

--- Bamminions/src/views/News/NewsLayoutView.vue
+++ Bamminions/src/views/News/NewsLayoutView.vue
@@ -10,6 +10,16 @@ const { news } = storeToRefs(store)
   <div v-if="news" class="p-12 text-gray-800 bg-gradient-to-b to-white">
     <nav class="mb-8 flex justify-center gap-4">
       <router-link
+        :to="{ name: 'news-view' }"
+        class="group relative w-50 h-25 rounded-2xl backdrop-blur shadow-xl ring-1 ring-black/5 overflow-hidden"
+      >
+        <span
+          class="pointer-events-none absolute inset-0 grid place-items-center font-semibold text-2xl md:text-3xl text-white"
+        >
+          Home
+        </span>
+      </router-link>
+      <router-link
         :to="{ name: 'news-detail-view', params: { id: $route.params.id } }"
         class="group relative w-50 h-25 rounded-2xl backdrop-blur shadow-xl ring-1 ring-black/5 overflow-hidden"
       >
@@ -19,6 +29,7 @@ const { news } = storeToRefs(store)
           Detail
         </span>
       </router-link>
+
       <router-link
         :to="{ name: 'news-vote-view', params: { id: $route.params.id } }"
         class="group relative w-50 h-25 rounded-2xl backdrop-blur shadow-xl ring-1 ring-black/5 overflow-hidden"
